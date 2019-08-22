6c4d73f5ec4889d8783d824797b367bec7a7708c
Chad Luong <apple.luong1905@gmail.com>
2019-08-22 19:03:00 +0700

    update

---
+++ session14/js/bt.js
@@ -0,0 +1,9 @@
+var a = prompt("Nhap so dau tien", "");
+var b = prompt("Nhap so thu 2", "");
+var c = prompt("Nhap so thu 3", "");
+document.write(a);
+document.write('<br>');
+document.write(b);
+document.write('<br>');
+document.write(c);
+document.write('<br>');
