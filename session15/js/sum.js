1b65ea3bd72e3a02d7ffa6bdb87b2554a05c75d6
Chad Luong <apple.luong1905@gmail.com>
2019-08-27 19:40:03 +0700

    update

---
+++ session15/js/sum.js
@@ -0,0 +1,8 @@
+function sumNumber() {
+	var number1, number2;
+	number1 = document.getElementById('number1').value;
+	number1 = parseInt(number1);
+	number2 = document.getElementById('number2').value;
+	number2 = parseInt(number2);
+	document.getElementById('sum').innerHTML = number1 + number2;
+}
